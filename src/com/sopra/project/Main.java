21b1434fce3befc163161f32d8b876cfd41b2e38
Training <Training@ITEM-S099882.emea.msad.sopra>
2022-09-22 15:53:14 +0530

    added deposit and withdraw methods

---
+++ src/com/sopra/project/Main.java
@@ -0,0 +1,21 @@
+package com.sopra.project;
+
+public class Main 
+{
+
+	public double deposit(int accnum,double amt)
+	{
+		return amt;
+	}
+	
+	public double withdraw(int accnum,double amt)
+	{
+		return amt;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
